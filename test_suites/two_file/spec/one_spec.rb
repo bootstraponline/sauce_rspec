e0e3ac3f7d731a3013b110a5b4667c513d4811e1
bootstraponline <code@bootstraponline.com>
2015-09-17 16:04:12 -0400

    Add spec-retry and test_suites

--- test_suites/two_file/spec/one_spec.rb
+++ test_suites/two_file/spec/one_spec.rb
@@ -3,5 +3,6 @@ require_relative 'spec_helper'
 describe 'one' do
   it 'one' do
     puts 'test one'
+    fail if rand > 0.50
   end
 end
--- test_suites/two_file/spec/two_spec.rb
+++ test_suites/two_file/spec/two_spec.rb
@@ -3,5 +3,6 @@ require_relative 'spec_helper'
 describe 'two' do
   it 'two' do
     puts 'test two'
+    fail if rand > 0.50
   end
 end
