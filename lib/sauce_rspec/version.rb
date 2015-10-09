52bcf9fd9dc970122a42fb7eb407de2a9ff37db5
bootstraponline <code@bootstraponline.com>
2015-10-08 23:54:21 -0400

    Release 1.0.1

--- lib/sauce_rspec/version.rb
+++ lib/sauce_rspec/version.rb
@@ -1,4 +1,4 @@
 module SauceRSpec
-  VERSION = '1.0.0' unless defined? ::SauceRSpec::VERSION
+  VERSION = '1.0.1' unless defined? ::SauceRSpec::VERSION
   DATE    = '2015-10-08' unless defined? ::SauceRSpec::DATE
 end
