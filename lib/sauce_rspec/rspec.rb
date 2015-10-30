5dad028b4c7cf3208cc9f52caa5d40bfea059421
bootstraponline <code@bootstraponline.com>
2015-10-30 16:43:16 -0400

    Fix nested describe support

--- lib/sauce_rspec/rspec.rb
+++ lib/sauce_rspec/rspec.rb
@@ -18,25 +18,24 @@ module RSpec
         config = ::SauceRSpec.config
         return rspec_register(example_group) unless config.sauce?
 
-        # must use `descendant_filtered_examples` to ensure all examples
-        # including those from nested example groups are included.
-        examples     = example_group.descendant_filtered_examples
-        new_examples = []
-        examples.each do |ex|
-          # Use index to allow multiple duplicate caps to have unique ids
-          # ex: 1_firefox, 2_firefox
-          config.caps.each_with_index do |cap, index|
-            ex_with_cap = ex.clone
-            new_id      = "#{ex_with_cap.id}_#{index}_#{cap}"
-            ex_with_cap.instance_variable_set(:@id, new_id)
-            # can *not* use metadata[:caps] because rspec will memoize the
-            # value and then reuse it for all the examples.
-            ex_with_cap.caps = cap
-            new_examples << ex_with_cap
+        # must iterate through descendants to handle nested describes
+        example_group.descendants.each do |desc_group|
+          new_examples = []
+          desc_group.examples.each do |ex|
+            # Use index to allow multiple duplicate caps to have unique ids
+            # ex: 1_firefox, 2_firefox
+            config.caps.each_with_index do |cap, index|
+              ex_with_cap = ex.clone
+              new_id      = "#{ex_with_cap.id}_#{index}_#{cap}"
+              ex_with_cap.instance_variable_set(:@id, new_id)
+              # can *not* use metadata[:caps] because rspec will memoize the
+              # value and then reuse it for all the examples.
+              ex_with_cap.caps = cap
+              new_examples << ex_with_cap
+            end
           end
+          desc_group.instance_variable_set(:@examples, new_examples)
         end
-
-        example_group.instance_variable_set(:@examples, new_examples)
 
         # invoke original register method
         rspec_register(example_group)
